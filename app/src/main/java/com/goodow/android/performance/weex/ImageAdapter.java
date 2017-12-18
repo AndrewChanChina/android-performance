dc91ded1745d1af4060d780409eba602fb4c3805
Larry Tin <dev@goodow.com>
2017-12-18 19:50:49 +0800

    集成 Weex

---
+++ app/src/main/java/com/goodow/android/performance/weex/ImageAdapter.java
@@ -0,0 +1,18 @@
+package com.goodow.android.performance.weex;
+
+import android.widget.ImageView;
+
+import com.taobao.weex.adapter.IWXImgLoaderAdapter;
+import com.taobao.weex.common.WXImageStrategy;
+import com.taobao.weex.dom.WXImageQuality;
+
+/**
+ * Created by larry on 2017/12/15.
+ */
+
+public class ImageAdapter implements IWXImgLoaderAdapter {
+  @Override
+  public void setImage(String url, ImageView view, WXImageQuality quality, WXImageStrategy strategy) {
+    //实现你自己的图片下载，否则图片无法显示。
+  }
+}
